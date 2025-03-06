8feadd71eee89d8c57ad0ba061253fce039b442e
ezouu <eddiezou7374@gmail.com>
2025-03-06 15:52:38 -0800

    fixed some bugs

--- update_projects.py
+++ update_projects.py
@@ -60,11 +60,12 @@ def get_description_file_content(repo_name):
 def parse_description(content, github_url):
     """Parse description.txt content into project details"""
     lines = content.split('\n')
+    default_image = 'images/default.jpg'
     project = {
         'title': '',
         'year': '',
         'description': '',
-        'image': 'images/default.jpg',  # Changed default image to default.jpg
+        'image': default_image,  # Default image path
         'github_url': github_url
     }
     
@@ -83,11 +84,17 @@ def parse_description(content, github_url):
             project['year'] = line.replace('year:', '').strip()
             current_field = None
         elif line.startswith('image:'):
-            # Only use the specified image if it exists in the images directory
+            # Get the specified image path
             image_path = line.replace('image:', '').strip()
+            # If it doesn't start with 'images/', add it
+            if not image_path.startswith('images/'):
+                image_path = f'images/{image_path}'
+            # Only use the specified image if it exists
             if os.path.exists(image_path):
                 project['image'] = image_path
-            current_field = None
+            else:
+                print(f"Image not found: {image_path}, using {default_image}")
+                project['image'] = default_image
         elif line.startswith('description:'):
             current_field = 'description'
             description_lines.append(line.replace('description:', '').strip())
@@ -144,11 +151,11 @@ def update_html(projects):
             # Add GitHub link if available
             if project.get('github_url'):
                 link = soup.new_tag('a', href=project['github_url'])
-                img = soup.new_tag('img', attrs={'src': project['image'], 'width': '160'})
+                img = soup.new_tag('img', src=project['image'], attrs={'width': '160'})
                 link.append(img)
                 img_div.append(link)
             else:
-                img = soup.new_tag('img', attrs={'src': project['image'], 'width': '160'})
+                img = soup.new_tag('img', src=project['image'], attrs={'width': '160'})
                 img_div.append(img)
                 
             img_cell.append(img_div)
@@ -185,9 +192,22 @@ def update_html(projects):
             new_tr.append(content_cell)
             projects_table.append(new_tr)
     
+    # Format the HTML with proper indentation and line breaks
+    formatted_html = str(soup)
+    formatted_html = formatted_html.replace('</tbody><tr>', '</tbody>\n<tr>')
+    formatted_html = formatted_html.replace('</tr></table>', '</tr>\n</table>')
+    formatted_html = formatted_html.replace('><tr>', '>\n<tr>')
+    formatted_html = formatted_html.replace('</tr><', '</tr>\n<')
+    formatted_html = formatted_html.replace('><td', '>\n<td')
+    formatted_html = formatted_html.replace('</td><', '</td>\n<')
+    formatted_html = formatted_html.replace('><div', '>\n<div')
+    formatted_html = formatted_html.replace('</div><', '</div>\n<')
+    formatted_html = formatted_html.replace('><p>', '>\n<p>')
+    formatted_html = formatted_html.replace('</p><', '</p>\n<')
+    
     # Write updated HTML back to file
     with open('index.html', 'w') as f:
-        f.write(str(soup))
+        f.write(formatted_html)
 
 def main():
     try:
